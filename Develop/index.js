b14d0050632f6eec61b5711b8cf936c0c7486e54
Ryan Darton <ryan@timemachinevideo.com>
2024-03-27 19:04:06 -0600

    Import the needed packages.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,6 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
