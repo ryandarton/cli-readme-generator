e26ce5f3a430f2da4882631a7efd04fd6ef5a686
Ryan Darton <ryan@timemachinevideo.com>
2024-03-27 20:27:56 -0600

    Correct path to generateMarkdown.js file.

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 const questions = [
   {
