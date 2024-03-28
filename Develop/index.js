6475edcdbfe6e4e8cbfc3154f0bf9b10062ba188
Ryan Darton <ryan@timemachinevideo.com>
2024-03-27 19:14:06 -0600

    Create array of questions for the user.

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,65 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown');
 
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Provide the Title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'motivation',
+    message: 'What was your motivation?',
+  },
+  {
+    type: 'input',
+    name: 'why',
+    message: 'Why did you build this project?',
+  },
+  {
+    type: 'input',
+    name: 'solve',
+    message: 'What problem does it solve?',
+  },
+  {
+    type: 'input',
+    name: 'lessons',
+    message: 'What did you learn?',
+  },
+  {
+    type: 'input',
+    name: 'imagePath',
+    message: 'Enter the file path of the PNG image to include in the README (e.g., ./path/to/image.png):',
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'List your collaborators, if any, with links to their GitHub profiles:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
