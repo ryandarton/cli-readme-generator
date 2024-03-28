42173fedfb611382ad8f9fb89be508cd705ab9d9
Ryan Darton <ryan@timemachinevideo.com>
2024-03-27 20:08:20 -0600

    First attempt at the generateMarkdown functionality.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+// A function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
     case 'MIT':
@@ -8,23 +10,52 @@ function renderLicenseBadge(license) {
       return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
     case 'BSD 3-Clause':
       return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+    case 'None':
+      return '';
     default:
       return '';
   }
 }
 
-// TODO: Create a function that returns the license link
+// A function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `## License
+  This project is licensed under the ${license} license.`;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// A function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+- ${data.motivation}
+- ${data.why}
+- ${data.solve}
+- ${data.lessons}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.installation}
+
+![Project Image](${data.imagePath})
+
+## Credits
+${data.credits}
+
+## Questions
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+
+${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
 `;
 }
 
