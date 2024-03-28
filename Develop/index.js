9b63f425b03a086a4e2a034c3cfef340b539f1f9
Ryan Darton <ryan@timemachinevideo.com>
2024-03-27 19:16:47 -0600

    Add functionality to use the data to create the readme file.

--- Develop/index.js
+++ Develop/index.js
@@ -61,11 +61,22 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md generated successfully!');
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readme = generateMarkdown(answers);
+    writeToFile('README.md', readme);
+  });
+}
 
 // Function call to initialize app
 init();
